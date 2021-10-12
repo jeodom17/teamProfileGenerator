7edccee0ffcee13ff214f42aa880e984479bd97b
jeodom17 <jeodom17@gmail.com>
2021-10-11 23:14:24 -0400

    finish testing

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+
+
+const rosterData = {
+    
+}
