f6fb3f90774ae7a87c111c47346cc1fc0260e1af
jeodom17 <jeodom17@gmail.com>
2021-10-11 23:43:22 -0400

    added egngineer and intern questions

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const rosterData = {
     engineerData: [],
 };
 
-const questionData = [
+const managerQuestions = [
     //* Manager questions
   {
     type: 'input',
     message: 'What is the Managers name?',
-    name: managerName,
+    name: 'managerName',
   },
   {
     type: 'input',
@@ -32,28 +32,51 @@ const questionData = [
     type: 'input',
     message: 'What is the Managers Office Number?',
     name: 'managerOfficeNum',
-  },
-     //* Engineer questions
+  }
+]
+  //* Engineer questions
+  const engineerQuestions = [
      {
         type: 'input',
-        message: 'What is the Managers name?',
-        name: managerName,
+        message: 'What is the Engineers name?',
+        name: 'engineerName',
       },
       {
         type: 'input',
-        message: 'What is the Managers Id?',
-        name: 'managerId',
+        message: 'What is the Engineer Id?',
+        name: 'engineerId',
       },
       {
         type: 'input',
-        message: 'What is the Managers email?',
-        name: 'managerEmail',
+        message: 'What is the Engineer email?',
+        name: 'engineerEmail',
       },
       {
         type: 'input',
-        message: 'What is the Managers Office Number?',
-        name: 'managerOfficeNum',
+        message: 'What is the Engineers github username?',
+        name: 'engineerGithub',
+      }
+    ]
+      //* Intern questions\
+  const internQuestions = [
+     {
+        type: 'input',
+        message: 'What is the Interns name?',
+        name: 'internName',
       },
-    
-
+      {
+        type: 'input',
+        message: 'What is the Intern Id?',
+        name: 'internId',
+      },
+      {
+        type: 'input',
+        message: 'What is the intern email?',
+        name: 'internEmail',
+      },
+      {
+        type: 'input',
+        message: 'What is the name of the interns School?',
+        name: 'internSchool',
+      },
 ]
